7a6a6175acd4c17d9d9a8262ddf3cc589a4155c9
Nguyễn Phương Nam <npnam.fit.hcmus@gmail.com>
2019-01-19 19:35:32 +0700

    update signup

--- home/views.py
+++ home/views.py
@@ -5,5 +5,7 @@ def index(request):
     return render(request, 'pages/home.html')
 def contact(request):
     return render(request, 'pages/contact.html')
+def register(request):
+    return render(request,'pages/register.html')
 def error(request):
     return render(request, 'pages/error.html')
